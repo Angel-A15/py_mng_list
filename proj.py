00844145d65102ff647518c52d2dd38d6965389e
Angel-A15 <aasupertwix15@gmail.com>
2024-12-11 01:05:43 -0600

    enabled function that will loop until we are finished editing and deleting data

--- proj.py
+++ proj.py
@@ -12,21 +12,45 @@ def add_person():
     person = {"name": name, "age": age, "email": email, "i.d.": id, "occupation": occupation}
     return person
 
+def delete_contact(people):
+    for i, person in enumerate(people):
+        print(i + 1, "-", person["name"], "|", person["age"], person["email"], person["i.d."], person["occupation"])
+    
+    while True:
+        number = input("Enter a number to delete: ")
+        try:
+            number = int(number)
+            if number <= 0 or number > len(people):
+                print("Invalid number, out of range.")
+            else:
+                break
+        except:
+            print("Invali number")
+
+    people.pop(number - 1)
+
+
 print("Hello, welcome to the Contact Management System.")
 print()
-command = input("You can 'Add', 'Delete', or 'Search: ").lower()
+
 people = []
 
-if command == "add":
-    person = add_person()
-    people.append(person)
-    print("Person added")
 
-elif command == "delete":
-    pass
-elif command == "search":
-    pass
-else:
-    print("Imvalid command.")
+while True:
+    command = input("You can 'Add', 'Delete', or 'Search and 'Q' to exit: ").lower()
+
+    if command == "add":
+        person = add_person()
+        people.append(person)
+        print("Person added")
+
+    elif command == "delete":
+        delete_contact(people)
+    elif command == "search":
+        pass
+    elif command == "q":
+        break
+    else:
+        print("Imvalid command.")
 
 print(people)
